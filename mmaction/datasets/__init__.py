09b1ca0260b006edb42f6bb3ac9070c3142f6860
zhaoyue-zephyrus <thuzhaoyue@gmail.com>
2019-04-20 00:11:16 +0800

    add lmdb dataset
    Former-commit-id: 672d8a3e85a9994c1723561c06c9b2db2b352c93

--- mmaction/datasets/__init__.py
+++ mmaction/datasets/__init__.py
@@ -1,9 +1,10 @@
 from .rawframes_dataset import RawFramesDataset
+from .lmdbframes_dataset import LMDBFramesDataset
 from .utils import get_untrimmed_dataset, get_trimmed_dataset
 from .loader import GroupSampler, DistributedGroupSampler, build_dataloader
 
 __all__ = [
-    'RawFramesDataset',
+    'RawFramesDataset', 'LMDBFramesDataset',
     'get_trimmed_dataset', 'get_untrimmed_dataset',
     'GroupSampler', 'DistributedGroupSampler', 'build_dataloader'
 ]
---
+++ tools/generate_lmdb.py
@@ -0,0 +1,38 @@
+import argparse
+import glob
+import os.path as osp
+from mmcv.lmdb.io import create_rawimage_dataset
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='generate lmdb datasets from raw frames')
+    parser.add_argument('root_dir', help='root directory to store the raw frames')
+    parser.add_argument('target_dir', help='target directory to stored the generated lmdb datasets')
+    parser.add_argument('--image_format', nargs='+', help='format of the images to be stored', default=['img*.jpg'])
+    parser.add_argument('--lmdb_tmpl', type=str, help='template for the lmdb to be generated', default='{}_img_lmdb')
+    parser.add_argument('--image_tmpl', type=str, help='template for the lmdb key', default=None)
+    parser.add_argument('--modality', type=str, help='modality', choices=['RGB', 'Flow'], default='RGB')
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+    video_path_list = glob.glob(osp.join(args.root_dir, '*'))
+    for i, vv in enumerate(video_path_list):
+        if not osp.isdir(vv):
+            continue
+        image_file_list = []
+        for image_format in args.image_format:
+            image_file_list += glob.glob(osp.join(vv, image_format)) 
+        vid = vv.split('/')[-1]
+        output_path = osp.join(args.target_dir, args.lmdb_tmpl.format(vid))
+        create_rawimage_dataset(output_path, image_file_list,
+                                image_tmpl=args.image_tmpl,
+                                flag='color' if args.modality == 'RGB' else 'grayscale',
+                                check_valid=True)
+
+
+if __name__ == '__main__':
+    main()
