292a7e9c485de54e0c3d40b9ce5ce37599924976
zhaoyue-zephyrus <thuzhaoyue@gmail.com>
2019-06-18 05:30:50 +0800

    fix typo

--- data_tools/ava/obtain_video_resolution.sh
+++ data_tools/ava/obtain_video_resolution.sh
@@ -4,8 +4,10 @@ cd ../../data/ava/
 
 ls ./video_trimmed_trainval | while read filename; do \
   vid="$(echo ${filename} | cut -d'.' -f1)";
-  resolution="ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 ${filename}"
+  resolution=`ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 ./video_trimmed_trainval/${filename}`
   echo ${vid} ${resolution}
 done &> ava_video_resolution_stats.csv
 
-cd ../../data_pool/ava/
+echo $PWD
+
+cd ../../data_tools/ava/
