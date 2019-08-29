cdf4da1a77d6c61dcf679eb2146b7ed73445a649
duanhaodong <duanhaodong@sensetime.com>
2019-08-29 14:54:23 +0800

    add TSN2D Kinetics

---
+++ configs/kinetics400/kinetics400_2d_rgb_r50_seg3.py
@@ -0,0 +1,129 @@
+model = dict(
+    type='TSN2D',
+    backbone=dict(
+        type='ResNet',
+        pretrained='modelzoo/resnet50-19c8e357.pth',
+        depth=50,
+        out_indices=(3,),
+        bn_eval=False,
+        partial_bn=False),
+    spatial_temporal_module=dict(
+        type='SimpleSpatialModule',
+        spatial_type='avg',
+        spatial_size=7),
+    segmental_consensus=dict(
+        type='SimpleConsensus',
+        consensus_type='avg'),
+    cls_head=dict(
+        type='ClsHead',
+        with_avg_pool=False,
+        temporal_feature_size=1,
+        spatial_feature_size=1,
+        dropout_ratio=0.4,
+        in_channels=2048,
+        num_classes=400))
+train_cfg = None
+test_cfg = None
+# dataset settings
+dataset_type = 'RawFramesDataset'
+data_root = '/mnt/SSD/Kinetics_extracted'
+data_root_val = '/mnt/SSD/Kinetics_extracted_val'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+data = dict(
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file='data/kinetics_train_list.txt',
+        videos_per_gpu=32,
+        img_prefix=data_root,
+        img_norm_cfg=img_norm_cfg,
+        num_segments=3,
+        new_length=1,
+        new_step=1,
+        random_shift=True,
+        modality='RGB',
+        image_tmpl='img_{:05d}.jpg',
+        img_scale=256,
+        input_size=224,
+        div_255=False,
+        flip_ratio=0.5,
+        resize_keep_ratio=True,
+        oversample=None,
+        random_crop=False,
+        more_fix_crop=False,
+        multiscale_crop=True,
+        scales=[1, 0.875, 0.75, 0.66],
+        max_distort=1,
+        test_mode=False),
+    val=dict(
+        type=dataset_type,
+        videos_per_gpu=6,
+        ann_file='data/kinetics_val_list.txt',
+        img_prefix=data_root_val,
+        img_norm_cfg=img_norm_cfg,
+        num_segments=25,
+        new_length=1,
+        new_step=1,
+        random_shift=False,
+        modality='RGB',
+        image_tmpl='img_{:05d}.jpg',
+        img_scale=256,
+        input_size=224,
+        div_255=False,
+        flip_ratio=0,
+        resize_keep_ratio=True,
+        oversample=None,
+        random_crop=False,
+        more_fix_crop=False,
+        multiscale_crop=False,
+        test_mode=False),
+    test=dict(
+        type=dataset_type,
+        ann_file='data/kinetics_val_list.txt',
+        videos_per_gpu=2,
+        img_prefix=data_root_val,
+        img_norm_cfg=img_norm_cfg,
+        num_segments=25,
+        new_length=1,
+        new_step=1,
+        random_shift=False,
+        modality='RGB',
+        image_tmpl='img_{:05d}.jpg',
+        img_scale=256,
+        input_size=224,
+        div_255=False,
+        flip_ratio=0,
+        resize_keep_ratio=True,
+        oversample="ten_crop",
+        random_crop=False,
+        more_fix_crop=False,
+        multiscale_crop=False,
+        test_mode=True))
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    step=[40, 80])
+checkpoint_config = dict(interval=5)
+workflow = [('train', 5), ('val', 1)]
+# workflow = [('train', 1)]
+# yapf:disable
+log_config = dict(
+    interval=20,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+# runtime settings
+total_epochs = 100
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = './work_dirs/dp04_wd14_float'
+load_from = None
+resume_from = None
+
