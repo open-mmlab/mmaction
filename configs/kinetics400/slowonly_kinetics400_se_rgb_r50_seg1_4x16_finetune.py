2b226a84652cd87b6628b4a770a001130fce18e9
Haodong Duan <34324155+kennymckormick@users.noreply.github.com>
2020-01-30 21:41:38 +0800

    Add distributed testing; add new augmentation and new models (#130)
    + Add distributed testing.
    
    + Add data augmentation: RandomResizeCrop; RandomRescaleCrop.
    
    + Add models: SlowFast; R(2+1)D; CSN.
    
    + update testing command in GETTING_STARTED.

---
+++ configs/kinetics400/slowonly_kinetics400_se_rgb_r50_seg1_4x16_finetune.py
@@ -0,0 +1,141 @@
+model = dict(
+    type='TSN3D',
+    backbone=dict(
+        type='ResNet_I3D',
+        pretrained='modelzoo://resnet50',
+        depth=50,
+        num_stages=4,
+        out_indices=[3],
+        frozen_stages=-1,
+        inflate_freq=(0, 0, 1, 1),
+        conv1_kernel_t=1,
+        conv1_stride_t=1,
+        pool1_kernel_t=1,
+        pool1_stride_t=1,
+        inflate_style='3x1x1',
+        bn_eval=False,
+        no_pool2=True,
+        partial_bn=False,
+        style='pytorch'),
+    spatial_temporal_module=dict(
+        type='SimpleSpatialTemporalModule',
+        spatial_type='avg',
+        temporal_size=4,
+        spatial_size=7),
+    segmental_consensus=dict(
+        type='SimpleConsensus',
+        consensus_type='avg'),
+    cls_head=dict(
+        type='ClsHead',
+        with_avg_pool=False,
+        temporal_feature_size=1,
+        spatial_feature_size=1,
+        dropout_ratio=0.5,
+        in_channels=2048,
+        num_classes=400))
+train_cfg = None
+test_cfg = None
+# dataset settings
+dataset_type = 'RawFramesDataset'
+data_root = 'data/kinetics400/rawframes_train/'
+data_root_val = 'data/kinetics400/rawframes_val/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+data = dict(
+    videos_per_gpu=16,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file='data/kinetics400/kinetics400_train_list_rawframes.txt',
+        img_prefix=data_root,
+        img_norm_cfg=img_norm_cfg,
+        input_format="NCTHW",
+        num_segments=1,
+        new_length=4,
+        new_step=16,
+        random_shift=True,
+        modality='RGB',
+        image_tmpl='img_{:05d}.jpg',
+        img_scale=256,
+        input_size=224,
+        div_255=False,
+        flip_ratio=0.5,
+        resize_crop=True,
+        resize_keep_ratio=True,
+        oversample=None,
+        random_crop=False,
+        more_fix_crop=False,
+        multiscale_crop=True,
+        test_mode=False),
+    val=dict(
+        type=dataset_type,
+        ann_file='data/kinetics400/kinetics400_val_list_rawframes.txt',
+        img_prefix=data_root_val,
+        img_norm_cfg=img_norm_cfg,
+        input_format="NCTHW",
+        num_segments=1,
+        new_length=4,
+        new_step=16,
+        random_shift=False,
+        modality='RGB',
+        image_tmpl='img_{:05d}.jpg',
+        img_scale=256,
+        input_size=224,
+        div_255=False,
+        flip_ratio=0,
+        resize_keep_ratio=True,
+        oversample=None,
+        random_crop=False,
+        more_fix_crop=False,
+        multiscale_crop=False,
+        test_mode=True),
+   test=dict(
+        type=dataset_type,
+        ann_file='data/kinetics400/kinetics400_val_list_rawframes.txt',
+        img_prefix=data_root_val,
+        img_norm_cfg=img_norm_cfg,
+        input_format="NCTHW",
+        num_segments=10,
+        new_length=4,
+        new_step=16,
+        random_shift=False,
+        modality='RGB',
+        image_tmpl='img_{:05d}.jpg',
+        img_scale=256,
+        input_size=256,
+        div_255=False,
+        flip_ratio=0,
+        resize_keep_ratio=True,
+        oversample='three_crop',
+        random_crop=False,
+        more_fix_crop=False,
+        multiscale_crop=False,
+        test_mode=True))
+
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    step=[90, 130],
+    warmup_ratio=0.01,
+    warmup='linear',
+    warmup_iters=18800)
+
+checkpoint_config = dict(interval=5)
+workflow = [('train', 1)]
+# yapf:disable
+log_config = dict(
+    interval=20,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+# runtime settings
+total_epochs = 150
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = './work_dirs/slowonly_kinetics400_se_rgb_r50_seg1_4x16_finetune_b16_g8'
+load_from = None
+resume_from = None
